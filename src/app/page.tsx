c30a08f1dc8cfc07a80771189253ba94221d3bd7
basavaraja-v <basavarajavyadav@gmail.com>
2024-04-19 18:03:12 +0530

    added ff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,60 +10,7 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const sampleDeals = [
-  {
-    "shopName": "HSR BDA Block Factory Outlets",
-    "headline": "30% to 70% off on formal and semi-formal wear",
-    "description": "Offers formal and semi-formal wear for men with discounts ranging from offers such as 30% to 70%, Buy One, Get One Free, and flat 70% discounts.",
-    "expirationDate": "2024-03-31",
-    "address": "HSR, Bengaluru",
-    "lat": 12.9116,
-    "lon": 77.6389
-  },
-  {
-    "shopName": "Style Union",
-    "headline": "₹200 Off for Your First Order",
-    "description": "Style Union is the ultimate fashion destination in Mysore, offering distinctive seasonal collections for women, men, and kids. Specializing in an exquisite line of Indian fashion clothing, they cater to all occasions.",
-    "expirationDate": "2024-03-31",
-    "address": "Garuda Mall, Albert Victor Rd, Agrahara, Mandi Mohalla, Mysore, KA 570001",
-    "lat": 12.3096,
-    "lon": 76.6553
-  },
-  {
-    "shopName": "Westside",
-    "headline": "Exclusive offers with Club West card",
-    "description": "Westside offers cutting-edge fashion and lifestyle brands for men, women, and children. Benefit from their Club West card, an exclusive rewards program designed for customers.",
-    "expirationDate": "2024-03-31",
-    "address": "No 560, New Kantharaj Urs Road, Next To Karnataka Bank, Kuvempunagara North, Saraswathipuram, Mysore, KA 570023",
-    "lat": 12.3051,
-    "lon": 76.6550
-  },
-  {
-    "shopName": "Fashion Factory",
-    "headline": "20-70% Discount on Best Brands",
-    "description": "Fashion Factory is India’s leading chain of fashion discount stores offering 20-70% discounts. They cater to every fashion taste, from trendy western wear to elegant Indian looks.",
-    "expirationDate": "2024-03-31",
-    "address": "Fashion Factory, 7JWQ+6FJ, Ashoka Cir, Chamarajapura, Chamarajapuram Mohalla, Krishnamurthy Puram, Mysuru, Karnataka 570009",
-    "lat": 12.2958,
-    "lon": 76.6394
-  },
-  {
-    shopName: "Reliance Fashion Factory",
-    "headline": "Fashion 365 days - 20-70% Discount on Best Brands",
-    "description": " Enjoy year-round shopping with exciting deals at Reliance Fashion Factory. Offering a range of trendy western wear to elegant Indian looks and everything in between. Shop your favourite brands for less",
-    "expirationDate": "2024-03-31",
-    "address": "nearest store in Bengaluru",
-    "lat": 12.971599,
-    "lon": 77.594566
-  },
-  {
-    shopName: "Jayanagar Cloth Shop",
-    "headline": "Save 50% at Jayanagar Cloth Shop",
-    "description": "Jayanagar Cloth Shop offers a wide range of readymade garments suitable for any occasion without upsetting the budget. This offer allows you to save significantly on your purchases",
-    "expirationDate": "2024-03-31",
-    "address": "62, 10 Main Road, 4th Block, Jaya Nagar, Jaya Nagar, 4th T Block East, Jayanagar 3rd Block, Bangalore",
-    "lat": 12.971599,
-    "lon": 77.594566
-  },
+
   {
     shopName: "KSIC Mysore Silk",
     "headline": "Up to 20% off on Mysore Silk Sarees",
@@ -74,77 +21,15 @@ const sampleDeals = [
     "lon": 76.6394
   },
   {
-    shopName: "Central",
-    "headline": "Happiness Sale: Up to 51% Discount",
-    "description": "Enjoy up to 51% discount on over 200 brands plus additional shopping vouchers on purchases at Central stores in Bengaluru. Terms & Conditions apply.",
-    "expirationDate": "2024-03-31",
-    "address": "JP Nagar, Residency Road, Bengaluru",
+    shopName: "Fashion Factory",
+    "headline": "We miss you at Fashion Factory!",
+    "description": "Claim your Special offer of Rs.300 OFF on minimum shopping of Rs.1499 over and above the ongoing offers on top apparel brands like Levi’s, Lee Cooper, Raymond and more! 🎉 \n Use Code FFRTUBSGQU3107 till 28th April.Don’t miss out on the chance to save big, visit our store & avail fantastic deals! 🛍️ \n📍relff.com / ffstores\n Happy Shopping! 🥳",
+    "expirationDate": "2024-04-28",
+    "address": "Ground Floor, Keerthana Complex, near Railway Under Bridge, Krishnamurthy Puram, Mysuru, Karnataka 570004",
     "lat": 12.971599,
     "lon": 77.594566
   },
-  {
-    shopName: "Unlimited",
-    "headline": "Fashionable Ugadi Sale: Items Start at Rs.149",
-    "description": "Celebrate Ugadi with fashionable offers from Unlimited Stores in Bengaluru. Laptop Bag at Rs.149 and more on qualifying purchases. 5% Instant Discount with SBI Card.",
-    "expirationDate": "2024-03-31",
-    "address": "Nearst Store",
-    "lat": 12.971599,
-    "lon": 77.594566
-  },
-  {
-    shopName: "Lifestyle",
-    "headline": "Sale: Boat Airdopes at Rs.949",
-    "description": "Get Boat Airdopes at Rs.949 on shopping of Rs.5,000 at Lifestyle Stores in Bengaluru. Also, avail Timex smart watch at a special price on shopping of Rs.10,000.",
-    "expirationDate": "2024-03-25",
-    "address": "Adarsh Opus, Forum Shantiniketan, Gopalan Signature Mall, and more locations in Bengaluru",
-    "lat": 12.971599,
-    "lon": 77.594566
-  },
-  {
-    shopName: 'Croma',
-    headline: 'UNLIMITED 5% off on Windows laptops above ₹50,000 Show code: CCLLAP58H2',
-    description: "Summer offers you just can't refuse! Use these special coupon code to get UNLIMITED 5% off on Windows laptops above ₹50,000",
-    expirationDate: '2024-03-31',
-    address: 'Nearest Croma Store',
-    lat: 12.295810,
-    lon: 76.639381
-  },
-  {
-    shopName: 'Croma',
-    headline: 'UNLIMITED 7% off on refrigerators Show code: CCLREF7N8I',
-    description: "Summer offers you just can't refuse! Use these special coupon code to get UNLIMITED 7% off on refrigerators",
-    expirationDate: '2024-03-31',
-    address: 'Nearest Croma Store',
-    lat: 12.295810,
-    lon: 76.639381
-  },
-  {
-    shopName: 'Croma',
-    headline: 'UNLIMITED 7% off on 2 or more split ACs Show code: CCLAC7D31',
-    description: "Summer offers you just can't refuse! Use these special coupon code to get UNLIMITED 7% off on 2 or more split ACs",
-    expirationDate: '2024-03-31',
-    address: 'Nearest Croma Store',
-    lat: 12.295810,
-    lon: 76.639381
-  },
-  {
-    shopName: 'Croma',
-    headline: 'UNLIMITED 5% off on 1 AC Show code: CCLAC54TK',
-    description: "Summer offers you just can't refuse! Use these special coupon code to get UNLIMITED 5% off on 1 AC",
-    expirationDate: '2024-03-31',
-    address: 'Nearest Croma Store',
-    lat: 12.295810,
-    lon: 76.639381
-  },
-  {
-    shopName: 'VK Fruits',
-    headline: 'Buy fruits up to ₹2000 to get 5% Off',
-    description: 'Enjoy a diverse selection of fruits, ranging from grapes and watermelon to mango and beyond.',
-    expirationDate: '2024-03-31',
-    address: 'VK Fruits, Kantharaj Urs road, Saraswathipuram, Mysore',
-    lat: 12.295810,
-    lon: 76.639381
-  },
+
 
 ];
 
